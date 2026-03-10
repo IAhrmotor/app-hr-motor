aee9219fd297ab830632c3b44c51f5fa7b5d6fec
Jarasa03 <javierarruabarrena03@gmail.com>
2026-03-10 10:32:17 +0100

    refactor: :recycle: Se cambian de sitio las urls para tenerlas centralizadas

---
+++ config/portal.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'name' => 'App HR Motor',
+
+    'links' => [
+        'it_support' => 'https://hrmotor.my.site.com/hrmotorcommunity/s/login/?ec=302&startURL=%2Fhrmotorcommunity%2Fs%2Frecordlist%2FTareas_Departamento_Informatico__c%2FDefault',
+
+        'tools' => [
+            'docusign' => 'https://account.docusign.com/oauth/auth?response_type=code&scope=all%20click.manage%20me_profile%20room_forms%20room_fields%20inproductcommunication_read%20data_explorer_signing_insights%20notary_read%20notary_write%20search_read%20search_write%20webforms_manage%20dtr%20valmod_manage%20spring_read%20spring_write%20signature&client_id=2CC56DC9-4BCD-4B55-8AB0-8BA60BAE1065&redirect_uri=https://apps.docusign.com/authenticate&state=%7b%22widgetId%22:%22%40ds/send%22%2c%22xsrfToken%22:%22IYIogBvwZtz%2BEZviKPqyJlr915WEM8vmFZ14xMfBM5jq%2BBofLnLQDzRb37YARzOUNCSgjxu5e3ub3V0hoZVtzDbLaz7JvwviWui6HrM8DIybiaUP6kTN162yJKtVehy4Vm5nUroRdbTTg0cV3qpGirh%2BH3owkhvZb9GPMPNPPCE%3D%22%2c%22redirectUri%22:%22/send/authentication?back%3D%252Fhome%22%2c%22authTxnId%22:%2217f86b96-dd9d-4f8d-8d26-f741b689b5c2%22%7d',
+            'google_drive' => 'https://drive.google.com',
+            'web_hr_motor' => 'https://www.hrmotor.com/',
+            'woffu' => 'https://hrmotor.woffu.com/v2/personal/dashboard/user',
+            'salesforce_comunidad' => 'https://hrmotor.my.site.com/hrmotorcommunity/s/login/?ec=302&startURL=%2Fhrmotorcommunity%2Fs%2F',
+            'lendismart' => 'https://hrmotor.lendismart.com/login',
+            'my_mutua' => 'https://access.mutua.es/auth/realms/mymutua/protocol/openid-connect/auth?response_type=code&client_id=appseguros-front-canal-mediacion&scope=openid&state=kmbjXkQ7kJGvwZLQYHuo6lrnl1g7xDf-l2dCRIdr04M%3D&redirect_uri=https://www.mymutua.es/front-canal-mediacion/login/oauth2/code/sso&nonce=L0XFsW7iFaW1ZhX6dGXDl-tQm1y89v-5n7mKoEBgpv4',
+            'formacion_comerciales' => 'https://formacion.hrmotor.com/my/courses.php',
+            'serviceform' => 'https://www.serviceform.es/',
+            'whatsapp_web' => 'https://web.whatsapp.com/',
+            'webmail' => 'http://mail.hrmotor.com/correoweb/?_task=mail&_mbox=INBOX',
+            'outlook' => 'https://outlook.office.com/',
+            'teams' => 'https://teams.microsoft.com/',
+            'onedrive' => 'https://www.office.com/launch/onedrive',
+            'word' => 'https://www.office.com/launch/word',
+            'excel' => 'https://www.office.com/launch/excel',
+            '3cx' => 'https://hrmotor.3cx.es:5001/#/chat',
+        ],
+    ],
+];
