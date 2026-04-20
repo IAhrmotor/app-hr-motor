7f14c21a424f31796e6d3257efa11ea69216d4e7
jarasa03 <javierarruabarrena03@gmail.com>
2026-04-20 14:47:49 +0200

    feat: :adhesive_bandage: Traducción de textos en la paginación

--- lang/es/pagination.php
+++ lang/es/pagination.php
@@ -7,10 +7,9 @@ return [
     | Líneas de idioma de paginación
     |--------------------------------------------------------------------------
     |
-    | Las siguientes líneas de idioma son utilizadas por la librería de
-    | paginación para construir los enlaces de paginación simple. Puedes
-    | cambiarlas por el texto que prefieras para adaptarlas mejor a tu
-    | aplicación.
+    | Las siguientes líneas de idioma las utiliza la librería de paginación
+    | para construir los enlaces de paginación simple. Puedes cambiarlas por
+    | el texto que prefieras para adaptarlas mejor a tu aplicación.
     |
     */
 
