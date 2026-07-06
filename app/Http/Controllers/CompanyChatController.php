d685e1802504744fefe095aef7f59f57c362457f
jarasa03 <javierarruabarrena03@gmail.com>
2026-07-06 12:34:57 +0200

    feat: :sparkles: Separación entre mensajes de personas diferentes en grupos

--- app/Http/Controllers/CompanyChatController.php
+++ app/Http/Controllers/CompanyChatController.php
@@ -553,9 +553,12 @@ class CompanyChatController extends Controller
         $limit = max(1, min((int) $request->integer('limit', self::CHAT_MESSAGES_PAGE_SIZE), 100));
         $beforeMessageId = $this->sanitizeMessageCursor($request->query('before_message_id'));
         $afterMessageId = $this->sanitizeMessageCursor($request->query('after_message_id'));
+        $markAsRead = $request->boolean('mark_read', true);
 
-        $readMessageIds = $this->markConversationAsRead($conversation, $authUser);
-        $this->markConversationNotificationsAsRead($conversation, $authUser);
+        $readMessageIds = $markAsRead ? $this->markConversationAsRead($conversation, $authUser) : [];
+        if ($markAsRead) {
+            $this->markConversationNotificationsAsRead($conversation, $authUser);
+        }
 
         if ($readMessageIds !== []) {
             $targetUserIds = $conversation->participantsFor($authUser)
@@ -1330,7 +1333,10 @@ class CompanyChatController extends Controller
             'edited_at' => $message->edited_at?->toIso8601String(),
             'deleted_at' => $message->deleted_at?->toIso8601String(),
             'created_at_label' => $currentLabel,
-            'show_time' => $forceShowTime || $nextDateKey !== $currentDateKey || $nextLabel !== $currentLabel,
+            'show_time' => $forceShowTime
+                || $nextDateKey !== $currentDateKey
+                || $nextLabel !== $currentLabel
+                || ($nextMessage !== null && $nextMessage->sender_id !== $message->sender_id),
             'read_at' => $message->read_at?->toIso8601String(),
             'attachments' => $this->formatAttachmentsForPayload($message),
         ];
