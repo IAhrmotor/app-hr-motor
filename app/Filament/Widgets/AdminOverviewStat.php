ee81ceec80e2be6d72ef6c25be9d994d3a9b0d88
jarasa03 <javierarruabarrena03@gmail.com>
2026-09-23 17:19:35 +0200

    feat: :lipstick: Mejoras visuales en el backoffice

---
+++ app/Filament/Widgets/AdminOverviewStat.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class AdminOverviewStat extends Stat
+{
+    protected string $view = 'filament.widgets.admin-overview-stat';
+}
